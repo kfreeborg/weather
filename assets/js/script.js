0dc1c97b8223694c93f71decbf74eb1378c7dca3
Kelsey Freeborg <kelsey.freeborg@gmail.com>
2020-11-01 23:12:48 -0700

    added readme and fixed bugs

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,4 @@
 var currentCityEl = document.querySelector("#city");
-var currentTempEl = document.querySelector(".temperature");
-var currentHumidityEl = document.querySelector(".humidity"); 4
-var currentWindEl = document.querySelector(".wind-speed");
-var currentUVEl = document.querySelector(".uv-index");
 var searchBtnEl = document.querySelector("#search-btn");
 var clearEl = document.getElementById("clear-history");
 var historyEl = document.querySelector("#search-history");
@@ -13,7 +9,7 @@ var lat;
 var lon;
 
 function loadWeather(city) {
-  var city = currentCityEl.value;
+  console.log("loadWeather", city);
 
   var forecastUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + apiKey;
 
@@ -24,15 +20,11 @@ function loadWeather(city) {
     .then(function (response) {
       var date = new Date();
       var forecastDays = response.list;
-
+      $('.forecast').empty();
       for (var i = 0; i < forecastDays.length; i++) {
-        //var day = Number(forecastDays[i].dt_txt.split('-')[2].split(' ')[0]);
-        //var hour = forecastDays[i].dt_txt.split('-')[2].split(' ')[1];
-
 
         if (forecastDays[i].dt_txt.indexOf("12:00:00") !== -1) {
           var tempF = Math.floor((forecastDays[i].main.temp - 273.15) * 1.80 + 32);
-
 
           var card = $("<div>").addClass("card col bg-light rounded text-dark m-1");
           var cardBody = $("<div>").addClass("card-body p-3 forecastBody")
@@ -42,8 +34,8 @@ function loadWeather(city) {
 
           var image = $("<img>").attr("src", "https://openweathermap.org/img/w/" + forecastDays[i].weather[0].icon + ".png")
 
-          cardBody.append(cityDate, image, temperature, humidity);
-          card.append(cardBody);
+          cardBody.empty().append(cityDate, image, temperature, humidity);
+          card.empty().append(cardBody);
           $(".forecast").append(card);
         }
       }
@@ -96,14 +88,14 @@ function loadWeather(city) {
             uvIndexDisplay.addClass("btn btn-warning");
           }
 
-          $(".uv-index").append(uvIndexDisplay.text(finalUv));
+          $(".uv-index").empty().append(uvIndexDisplay.text(finalUv));
         })
     })
 };
 
 searchBtnEl.addEventListener("click", function () {
-  event.preventDefault();
-  var city = currentCityEl.value;
+  //event.preventDefault();
+  let city = currentCityEl.value;
   loadWeather(city);
   searchHistory.push(city);
   localStorage.setItem("search", JSON.stringify(searchHistory));
@@ -113,7 +105,7 @@ searchBtnEl.addEventListener("click", function () {
 function renderSearchHistory() {
   historyEl.innerHTML = "";
   for (var i = 0; i < searchHistory.length; i++) {
-    var historyItem = document.createElement("input");
+    let historyItem = document.createElement("input");
     historyItem.setAttribute("type", "text");
     // historyItem.setAttribute("readonly", true);
     historyItem.setAttribute("class", "form-control d-block bg-white");
@@ -123,10 +115,6 @@ function renderSearchHistory() {
 
     })
     historyEl.append(historyItem);
-
-    // if (searchHistory.length > 0) {
-    //   loadWeather(searchHistory[searchHistory.length - 1]);
-    // }
   }
 };
 
@@ -136,19 +124,4 @@ clearEl.addEventListener("click", function () {
   renderSearchHistory();
 });
 
-
-// GIVEN a weather dashboard with form inputs
-// WHEN I search for a city
-// THEN I am presented with current and future conditions for that city and that city is added to the search history
-
-
-// WHEN I view current weather conditions for that city
-// THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
-
-
-// WHEN I view the UV index
-// THEN I am presented with a color that indicates whether the conditions are favorable, moderate, or severe
-
-
-// WHEN I click on a city in the search history
-// THEN I am again presented with current and future conditions for that city
+renderSearchHistory();
