30ccb300398f54feb373c9429ce3bb6281e3b641
Kelsey Freeborg <kelsey.freeborg@gmail.com>
2020-11-01 22:14:08 -0700

    uv badge updated

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,7 +7,6 @@ var searchBtnEl = document.querySelector("#search-btn");
 var clearEl = document.getElementById("clear-history");
 var historyEl = document.querySelector("#search-history");
 var searchHistory = JSON.parse(localStorage.getItem("search")) || [];
-console.log(searchHistory);
 
 var apiKey = "db9e17c61d09c41192d5879ee37e6413";
 var lat;
@@ -16,20 +15,69 @@ var lon;
 function loadWeather(city) {
   var city = currentCityEl.value;
 
-  var todayUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey;
   var forecastUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + apiKey;
 
+  $.ajax({
+    url: forecastUrl,
+    method: "GET"
+  })
+    .then(function (response) {
+      var date = new Date();
+      var forecastDays = response.list;
+
+      for (var i = 0; i < forecastDays.length; i++) {
+        //var day = Number(forecastDays[i].dt_txt.split('-')[2].split(' ')[0]);
+        //var hour = forecastDays[i].dt_txt.split('-')[2].split(' ')[1];
+
+
+        if (forecastDays[i].dt_txt.indexOf("12:00:00") !== -1) {
+          var tempF = Math.floor((forecastDays[i].main.temp - 273.15) * 1.80 + 32);
+
+
+          var card = $("<div>").addClass("card col bg-light rounded text-dark m-1");
+          var cardBody = $("<div>").addClass("card-body p-3 forecastBody")
+          var cityDate = $("<h4>").addClass("card-title").text(date.toLocaleDateString('en-US'));
+          var temperature = $("<p>").addClass("card-text forecastTemp").text("Temperature: " + tempF + " °F");
+          var humidity = $("<p>").addClass("card-text forecastHumidity").text("Humidity: " + forecastDays[i].main.humidity + "%");
+
+          var image = $("<img>").attr("src", "https://openweathermap.org/img/w/" + forecastDays[i].weather[0].icon + ".png")
+
+          cardBody.append(cityDate, image, temperature, humidity);
+          card.append(cardBody);
+          $(".forecast").append(card);
+        }
+      }
+    })
+
+  var todayUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey;
 
   $.ajax({
     url: todayUrl,
     method: "GET"
   })
+
     .then(function (response) {
-      loadCurrentConditions(response);
+      var date = new Date();
+
+      var tempF = Math.floor((response.main.temp - 273.15) * 1.80 + 32);
+      $(".temperature").text(tempF + "°F");
+
+      var cityDate = date.toLocaleDateString("en-US");
+      $("#today_icon_div").attr({
+        "src": "http://openweathermap.org/img/w/" + response.weather[0].icon + ".png",
+        "height": "100px", "width": "100px"
+      });
+      var name = response.name;
+      $(".city").text(name + " " + cityDate);
+
+      var humidity = response.main.humidity;
+      $(".humidity").text(humidity + "%");
+
+      var windSpeed = response.wind.speed
+      $(".wind-speed").text(windSpeed + "MPH");
 
       var lat = response.coord.lat;
       var lon = response.coord.lon;
-      console.log(lat, lon)
 
       var uvUrl = "https://api.openweathermap.org/data/2.5/uvi/forecast?lat=" + lat + "&lon=" + lon + "&appid=" + apiKey + "&cnt=1";
 
@@ -39,78 +87,21 @@ function loadWeather(city) {
       })
         .then(function (uvIndex) {
           var uvIndexDisplay = $("<button>");
-          uvIndexDisplay.addClass("btn btn-danger");
-          $(".uv-index").append(uvIndexDisplay.text(uvIndex[0].value));
+          var finalUv = uvIndex[0].value;
+          if (finalUv > 7) {
+            uvIndexDisplay.addClass("btn btn-danger");
+          } else if (finalUv < 4) {
+            uvIndexDisplay.addClass("btn btn-success");
+          } else {
+            uvIndexDisplay.addClass("btn btn-warning");
+          }
+
+          $(".uv-index").append(uvIndexDisplay.text(finalUv));
         })
     })
-
-  $.ajax({
-    url: forecastUrl,
-    method: "GET"
-  })
-    .then(function (response) {
-      loadForecast(response);
-    })
 };
 
-
-
-// today's weather
-function loadCurrentConditions(response) {
-  var date = new Date();
-
-  var tempF = Math.floor((response.main.temp - 273.15) * 1.80 + 32);
-  $(".temperature").append(tempF + "°F");
-
-  var cityDate = date.toLocaleDateString("en-US");
-  var image = $("#today_icon_div").attr({
-    "src": "http://openweathermap.org/img/w/" + response.weather[0].icon + ".png",
-    "height": "100px", "width": "100px"
-  });
-  var name = response.name;
-  $(".city").append(name + " " + cityDate);
-
-  var humidity = response.main.humidity;
-  $(".humidity").append(humidity + "%");
-
-  var windSpeed = response.wind.speed
-  $(".wind-speed").append(windSpeed + "MPH");
-
-
-}
-
-// forecast
-function loadForecast(response) {
-  var date = new Date();
-  var forecastDays = response.list;
-
-  for (var i = 0; i < forecastDays.length; i++) {
-    var day = Number(forecastDays[i].dt_txt.split('-')[2].split(' ')[0]);
-    var hour = forecastDays[i].dt_txt.split('-')[2].split(' ')[1];
-    // console.log(day);
-    // console.log(hour);
-
-    if (forecastDays[i].dt_txt.indexOf("12:00:00") !== -1) {
-      var tempF = Math.floor((forecastDays[i].main.temp - 273.15) * 1.80 + 32);
-
-
-      var card = $("<div>").addClass("card col bg-light rounded text-dark m-1");
-      var cardBody = $("<div>").addClass("card-body p-3 forecastBody")
-      var cityDate = $("<h4>").addClass("card-title").text(date.toLocaleDateString('en-US'));
-      var temperature = $("<p>").addClass("card-text forecastTemp").text("Temperature: " + tempF + " °F");
-      var humidity = $("<p>").addClass("card-text forecastHumidity").text("Humidity: " + forecastDays[i].main.humidity + "%");
-
-      var image = $("<img>").attr("src", "https://openweathermap.org/img/w/" + forecastDays[i].weather[0].icon + ".png")
-
-      cardBody.append(cityDate, image, temperature, humidity);
-      card.append(cardBody);
-      $(".forecast").append(card);
-    }
-  }
-};
-
-
-searchBtnEl.addEventListener("click", function (event) {
+searchBtnEl.addEventListener("click", function () {
   event.preventDefault();
   var city = currentCityEl.value;
   loadWeather(city);
